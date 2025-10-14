d88c7bc1abac75319619dce5ca65c830b3e35c19
Yohel <yohelbastian.oyarzun@alumnos.ulagos.cl>
2025-10-14 16:48:25 -0300

    Ultimos retoques 14-10-2025

--- files/JS/a.js
+++ files/JS/a.js
@@ -10,32 +10,34 @@ function suma(){
     document.getElementById("resultado").value=res;
     alert("El resultado es: "+res);
 }
-function validar() {
+function login() {
     const user = document.getElementById("usuario");
     const form = document.getElementById("login");
     const pass = document.getElementById("contraseña");
     const errorUsuario = document.getElementById("error-usuario");
     const errorContraseña = document.getElementById("error-contraseña");
-
+    const caracterInvalido = /[^a-zA-Z0-9]/;
     form.addEventListener("submit", (e) => {
+        e.preventDefault();
         let mensajesErrorUsuario = [];
         let mensajesErrorContraseña = [];
 
         if (user.value.trim() === "") {
             mensajesErrorUsuario.push("El campo usuario no puede estar vacío");
-        } else if (user.value.length > 3) {
-            mensajesErrorUsuario.push("El usuario no puede tener más de 3 caracteres");
+        } else if (user.value.length < 7) {
+            mensajesErrorUsuario.push("El usuario debe tener al menos 7 caracteres");
         }
 
         if (pass.value.trim() === "") {
             mensajesErrorContraseña.push("El campo contraseña no puede estar vacío");
-        } else if (pass.value.length < 6) {
-            mensajesErrorContraseña.push("La contraseña debe tener al menos 6 caracteres");
+        } else {
+            if (pass.value.length < 6) {
+                mensajesErrorContraseña.push("La contraseña debe tener al menos 6 caracteres");
+            }
+            if (caracterInvalido.test(pass.value)) {
+                mensajesErrorContraseña.push("La contraseña contiene caracteres no válidos");
+            }
         }
-
-        errorUsuario.textContent = mensajesErrorUsuario.join(", ");
-        errorContraseña.textContent = mensajesErrorContraseña.join(", ");
-
         if (mensajesErrorUsuario.length > 0 || mensajesErrorContraseña.length > 0) {
             let todosLosErrores = mensajesErrorUsuario.concat(mensajesErrorContraseña).join("\n");
             alert(todosLosErrores);
@@ -43,7 +45,9 @@ function validar() {
             alert("Inicio de sesión exitoso");
             form.reset();
         }
-
-        e.preventDefault();
-    });
+    });   
 }
+window.onload = function() {
+    login();
+    suma();
+};
